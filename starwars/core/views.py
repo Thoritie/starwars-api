c8e6d553253a10412b38fed3959b9a89505dc62d
thoritie <thohirh9@gmail.com>
2020-01-11 10:00:30 +0700

    :sparkles: People List view :snowman:

--- starwars/core/views.py
+++ starwars/core/views.py
@@ -1,3 +1,17 @@
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
+from django.views.generic.base import TemplateView
 
-# Create your views here.
+from core.models import People
+
+
+class PeopleListView(TemplateView):
+    def get(self, request):
+        people = People.objects.first()
+        person = {
+            'name': people.name,
+            'gender': people.gender,
+            'homeworld': people.homeworld.name
+        }
+
+        return JsonResponse(person)
