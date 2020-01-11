2a18920f3c4f677d9c11384e2337b964e4925c28
thoritie <thohirh9@gmail.com>
2020-01-11 11:37:41 +0700

    :sparkles: Query by gender :snowman:

--- starwars/core/views.py
+++ starwars/core/views.py
@@ -2,6 +2,9 @@ from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
 
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
 from core.models import People
 
 
@@ -15,3 +18,25 @@ class PeopleListView(TemplateView):
         }
 
         return JsonResponse(person)
+
+class PeopleListApiView(APIView):
+    def get(self, request):
+        gender = request.query_params.get(
+            'gender', None
+        )
+
+        if gender is not None:
+            peoples = People.objects.filter(gender=gender)
+        else:
+            peoples = People.objects.all()
+
+        people_list = []
+        for people in  peoples:
+            people = {
+                'name': people.name,
+                'gender': people.gender,
+                'homeworld': people.homeworld.name
+            }
+            people_list.append(people)
+
+        return Response(people_list)
