84abaca22c4202f3268f2d83ecef9b53b858e118
thoritie <thohirh9@gmail.com>
2020-01-11 11:37:08 +0700

    :sparkles: Add api url :snowman:

--- starwars/starwars/urls.py
+++ starwars/starwars/urls.py
@@ -14,11 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
-from core.views import PeopleListView
+from core.views import PeopleListView, PeopleListApiView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('peoples/', PeopleListView.as_view())
+    path('peoples/', PeopleListView.as_view()),
+    path('api-auth/', include('rest_framework.urls')),
+    path('api/peoples/', PeopleListApiView.as_view())
 ]
